673d816f0b02825212c0390486e6a63af24b3419
Guido Knips <guido.knips@posteo.de>
2020-12-05 12:57:46 +0100

    Add solution for day 5, part 1

---
+++ day-05/src/main.rs
@@ -0,0 +1,85 @@
+use std::env;
+use std::fs::read_to_string;
+use std::path::Path;
+
+fn main() -> Result<(), String> {
+    let filename = env::args()
+        .nth(1)
+        .ok_or_else(|| "No file name given.".to_owned())?;
+    let content = read_to_string(&Path::new(&filename)).map_err(|e| e.to_string())?;
+    let lines: Vec<&str> = content.split('\n').filter(|l| !l.is_empty()).collect();
+
+    let seat_ids = lines
+        .iter()
+        .enumerate()
+        .map(|(i, l)| line_to_number(l, i))
+        .collect::<Result<Vec<u16>, String>>()?;
+    let max_seat_id = seat_ids
+        .iter()
+        .max()
+        .ok_or_else(|| "No seat IDs in input".to_owned())?;
+
+    println!("The highest seat ID is {}", max_seat_id);
+
+    Ok(())
+}
+
+fn line_to_number(line: &str, line_index: usize) -> Result<u16, String> {
+    if line.len() != 10 {
+        return Err(format!("Line #{} is not 10 bytes long", line_index));
+    }
+    let mut n: u16 = 0;
+    for c in line.chars() {
+        n <<= 1;
+        let bit = match c {
+            'F' => 0,
+            'B' => 1,
+            'R' => 1,
+            'L' => 0,
+            _ => return Err(format!("Unknown character '{}' in line #{}", c, line_index)),
+        };
+        n += bit;
+    }
+    Ok(n)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn line_to_number_should_parse_correctly() {
+        // given
+        let line = "FBFBBFFRLR";
+
+        // when
+        let result = line_to_number(line, 42);
+
+        // then
+        assert_eq!(result, Ok(357));
+    }
+
+    #[test]
+    fn line_to_number_should_fail_for_invalid_length() {
+        // given
+        let line = "FBFBFBFBFBF";
+
+        // when
+        let result = line_to_number(line, 42);
+
+        // then
+        assert_eq!(result, Err("Line #42 is not 10 bytes long".to_owned()));
+    }
+
+    #[test]
+    fn line_to_number_should_fail_for_unknown_characters() {
+        // given
+        let line = "FBFBÖFBFR";
+
+        // when
+        let result = line_to_number(line, 42);
+
+        // then
+        assert_eq!(result, Err("Unknown character 'Ö' in line #42".to_owned()));
+    }
+}
