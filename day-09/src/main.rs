298556e8c02d909c7d6a79c5c3d9f217e8ccb761
Guido Knips <guido.knips@posteo.de>
2020-12-09 10:25:28 +0100

    Add solution for day 9, part 2

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -11,6 +11,11 @@ fn main() -> Result<(), String> {
 
     if let Some(invalid_number) = find_first_invalid_number(&input, 25) {
         println!("First invalid number is {}", invalid_number);
+        if let Some(weakness) = find_weakness(&input, invalid_number) {
+            println!("Weakness is: {}", weakness);
+        } else {
+            println!("Found no weakness");
+        }
     } else {
         println!("There is no invalid number.");
     }
@@ -39,6 +44,32 @@ fn number_valid(preamble: &[u64], number: u64) -> bool {
     false
 }
 
+fn cum_sum(data: &[u64]) -> Vec<u64> {
+    data.iter()
+        .fold((Vec::with_capacity(data.len()), 0), |(mut v, sum), num| {
+            v.push(sum + num);
+            (v, sum + num)
+        })
+        .0
+}
+
+fn find_weakness(data: &[u64], invalid_number: u64) -> Option<u64> {
+    let data_sum = cum_sum(data);
+    // You know what, let's brute force the rest again
+    // Oh, and words cannot describe how relieved I am that I did not get an off-by-one-error.
+    for window_len in 2..data_sum.len() {
+        for lower in 0..(data_sum.len() - window_len) {
+            let upper = lower + window_len;
+            if data_sum[upper] - data_sum[lower] == invalid_number {
+                let min = data[lower..(upper + 1)].iter().min()?;
+                let max = data[lower..(upper + 1)].iter().max()?;
+                return Some(min + max);
+            }
+        }
+    }
+    None
+}
+
 fn parse_input(input: &str) -> Result<Vec<u64>, String> {
     input
         .split_terminator('\n')
@@ -54,6 +85,18 @@ mod test {
     use super::*;
 
     #[test]
+    fn cum_sum_calculates_cumulative_sum() {
+        // given
+        let data = [1, 2, 3, 4];
+
+        // when
+        let sum = cum_sum(&data);
+
+        // then
+        assert_eq!(&sum, &[1, 3, 6, 10])
+    }
+
+    #[test]
     fn find_first_invalid_number_works_for_example() {
         // given
         let data = [
@@ -67,5 +110,21 @@ mod test {
 
         // then
         assert_eq!(result, Some(127));
+    }
+
+    #[test]
+    fn find_weakness_works_for_example() {
+        // given
+        let data = [
+            35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309,
+            576,
+        ];
+        let invalid_number = 127;
+
+        // when
+        let result = find_weakness(&data, invalid_number);
+
+        // then
+        assert_eq!(result, Some(62))
     }
 }
