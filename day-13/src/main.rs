7c92ce48bbd16f7de6f4faa806a8cb7534ccbf38
Guido Knips <guido.knips@posteo.de>
2020-12-13 10:03:29 +0100

    Add solution for day 13, part 1

---
+++ day-13/src/main.rs
@@ -0,0 +1,71 @@
+use std::env;
+use std::fs::read_to_string;
+use std::path::Path;
+
+fn main() -> Result<(), String> {
+    let filename = env::args()
+        .nth(1)
+        .ok_or_else(|| "No file name given.".to_owned())?;
+    let content = read_to_string(&Path::new(&filename)).map_err(|e| e.to_string())?;
+
+    let (earliest_departure, bus_ids) = parse_without_x(&content)?;
+    let (next_bus_id, next_bus_timestamp) =
+        find_earliest_bus_after_timestamp(earliest_departure, &bus_ids)
+            .ok_or_else(|| "No busses arrive at all!".to_owned())?;
+    println!(
+        "The next bus id times the waiting time is {}",
+        next_bus_id * (next_bus_timestamp - earliest_departure)
+    );
+
+    Ok(())
+}
+
+fn find_earliest_bus_after_timestamp(timestamp: u64, bus_ids: &[u64]) -> Option<(u64, u64)> {
+    bus_ids
+        .iter()
+        .map(|id| (*id, (timestamp / id + 1) * id))
+        .min_by_key(|(_, ts)| *ts)
+}
+
+fn parse_without_x(input: &str) -> Result<(u64, Vec<u64>), String> {
+    let mut lines = input.split_terminator('\n');
+    let earliest_departure: u64 = lines
+        .next()
+        .expect("Expected at least one line")
+        .parse()
+        .map_err(|e| format!("Unable to parse earliest departure timestamp: {}", e))?;
+    let bus_ids_string = lines
+        .next()
+        .ok_or_else(|| "No bus IDs in input".to_owned())?;
+    let bus_ids = parse_bus_ids_without_x(bus_ids_string)?;
+
+    Ok((earliest_departure, bus_ids))
+}
+
+fn parse_bus_ids_without_x(line: &str) -> Result<Vec<u64>, String> {
+    line.split(',')
+        .filter(|s| s != &"x")
+        .map(|s| {
+            s.parse()
+                .map_err(|e| format!("Unable to parse bus id {}: {}", s, e))
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn find_earliest_bus_after_timestamp_works_for_example() {
+        // given
+        let (earliest_departure, bus_ids) =
+            parse_without_x("939\n7,13,x,x,59,x,31,19\n").expect("Expected valid example input");
+
+        // when
+        let result = find_earliest_bus_after_timestamp(earliest_departure, &bus_ids);
+
+        // then
+        assert_eq!(result, Some((59, 944)));
+    }
+}
