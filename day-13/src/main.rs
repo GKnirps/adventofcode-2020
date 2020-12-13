0e928dd54d327cc3943761933bb134d98ae1d1ad
Guido Knips <guido.knips@posteo.de>
2020-12-13 10:14:15 +0100

    Add parsing of oos bus ids to day 13

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), String> {
         .ok_or_else(|| "No file name given.".to_owned())?;
     let content = read_to_string(&Path::new(&filename)).map_err(|e| e.to_string())?;
 
-    let (earliest_departure, bus_ids) = parse_without_x(&content)?;
+    let (earliest_departure, bus_ids) = parse_without(&content)?;
     let (next_bus_id, next_bus_timestamp) =
         find_earliest_bus_after_timestamp(earliest_departure, &bus_ids)
             .ok_or_else(|| "No busses arrive at all!".to_owned())?;
@@ -20,14 +20,18 @@ fn main() -> Result<(), String> {
     Ok(())
 }
 
-fn find_earliest_bus_after_timestamp(timestamp: u64, bus_ids: &[u64]) -> Option<(u64, u64)> {
+fn find_earliest_bus_after_timestamp(
+    timestamp: u64,
+    bus_ids: &[Option<u64>],
+) -> Option<(u64, u64)> {
     bus_ids
         .iter()
-        .map(|id| (*id, (timestamp / id + 1) * id))
+        .filter_map(|id| *id)
+        .map(|id| (id, (timestamp / id + 1) * id))
         .min_by_key(|(_, ts)| *ts)
 }
 
-fn parse_without_x(input: &str) -> Result<(u64, Vec<u64>), String> {
+fn parse_without(input: &str) -> Result<(u64, Vec<Option<u64>>), String> {
     let mut lines = input.split_terminator('\n');
     let earliest_departure: u64 = lines
         .next()
@@ -37,17 +41,21 @@ fn parse_without_x(input: &str) -> Result<(u64, Vec<u64>), String> {
     let bus_ids_string = lines
         .next()
         .ok_or_else(|| "No bus IDs in input".to_owned())?;
-    let bus_ids = parse_bus_ids_without_x(bus_ids_string)?;
+    let bus_ids = parse_bus_ids(bus_ids_string)?;
 
     Ok((earliest_departure, bus_ids))
 }
 
-fn parse_bus_ids_without_x(line: &str) -> Result<Vec<u64>, String> {
+fn parse_bus_ids(line: &str) -> Result<Vec<Option<u64>>, String> {
     line.split(',')
-        .filter(|s| s != &"x")
         .map(|s| {
-            s.parse()
-                .map_err(|e| format!("Unable to parse bus id {}: {}", s, e))
+            if s == "x" {
+                Ok(None)
+            } else {
+                s.parse::<u64>()
+                    .map(Some)
+                    .map_err(|e| format!("Unable to parse bus id {}: {}", s, e))
+            }
         })
         .collect()
 }
@@ -60,7 +68,7 @@ mod test {
     fn find_earliest_bus_after_timestamp_works_for_example() {
         // given
         let (earliest_departure, bus_ids) =
-            parse_without_x("939\n7,13,x,x,59,x,31,19\n").expect("Expected valid example input");
+            parse_without("939\n7,13,x,x,59,x,31,19\n").expect("Expected valid example input");
 
         // when
         let result = find_earliest_bus_after_timestamp(earliest_departure, &bus_ids);
