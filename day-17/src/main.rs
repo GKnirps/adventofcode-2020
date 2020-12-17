d5eb86e2c47d3eefd75570c2b03a7a84542b445a
Guido Knips <guido.knips@posteo.de>
2020-12-17 11:45:33 +0100

    Add solution for day 17, part 2

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -13,17 +13,25 @@ fn main() -> Result<(), String> {
     // just add enough padding left and right to last for at least six generations without overflowing
     let initial_world_for_part_1 = create_world_with_centered_plane(&input_plane, input_width, 6);
     let world_after_6_gen = run_generations(initial_world_for_part_1, 6);
-    let alive_count_after_6_gen = count_alive_cells(&world_after_6_gen);
+    let alive_count_after_6_gen = count_alive_cells(&world_after_6_gen.cells);
     println!(
         "After six generations, {} cubes are active",
         alive_count_after_6_gen
     );
 
+    let initial_world_for_part_2 = create_world4d_with_centered_plane(&input_plane, input_width, 6);
+    let world4d_after_6_gen = run_generations4d(initial_world_for_part_2, 6);
+    let alive_count_4d_after_6_gen = count_alive_cells(&world4d_after_6_gen.cells);
+    println!(
+        "After six generations, {} cubes are active in the 4-dimensional space",
+        alive_count_4d_after_6_gen
+    );
+
     Ok(())
 }
 
-fn count_alive_cells(world: &World) -> usize {
-    world.cells.iter().filter(|c| **c).count()
+fn count_alive_cells(cells: &[bool]) -> usize {
+    cells.iter().filter(|c| **c).count()
 }
 
 fn run_generations(initial_world: World, generations: usize) -> World {
@@ -113,6 +121,105 @@ fn create_world_with_centered_plane(
     }
 }
 
+fn run_generations4d(initial_world: World4d, generations: usize) -> World4d {
+    let mut world = initial_world;
+    for _ in 0..generations {
+        world = next_gen4d(&world);
+    }
+    world
+}
+
+fn next_gen4d(world: &World4d) -> World4d {
+    let next_cells: Vec<bool> = world
+        .cells
+        .iter()
+        .enumerate()
+        .map(|(i, alive)| {
+            let alive_neighbours = count_alive_neighbours4d(world, i as isize);
+            alive_neighbours == 3 || (alive_neighbours == 2 && *alive)
+        })
+        .collect();
+
+    World4d {
+        cells: next_cells,
+        size_x: world.size_x,
+        size_y: world.size_y,
+        size_z: world.size_z,
+        size_w: world.size_w,
+    }
+}
+
+fn count_alive_neighbours4d(world: &World4d, cell_index: isize) -> u8 {
+    let mut count = 0;
+
+    for dw in -1..=1 {
+        for dz in -1..=1 {
+            for dy in -1..=1 {
+                for dx in -1..=1 {
+                    if dx == 0 && dy == 0 && dz == 0 && dw == 0 {
+                        continue;
+                    }
+                    let index = cell_index
+                        + dw * world.size_z as isize
+                            * world.size_y as isize
+                            * world.size_x as isize
+                        + dz * world.size_y as isize * world.size_x as isize
+                        + dy * world.size_x as isize
+                        + dx;
+                    if index > 0
+                        && (index as usize) < world.cells.len()
+                        && world.cells[index as usize]
+                    {
+                        count += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    count
+}
+
+#[derive(Clone, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
+struct World4d {
+    cells: Vec<bool>,
+    size_x: usize,
+    size_y: usize,
+    size_z: usize,
+    size_w: usize,
+}
+
+fn create_world4d_with_centered_plane(
+    plane_cells: &[bool],
+    plane_width: usize,
+    padding: usize,
+) -> World4d {
+    let size_x = plane_width + 2 * padding;
+    let size_y = plane_cells.len() / plane_width + 2 * padding;
+    let size_z = 1 + 2 * padding;
+    let size_w = 1 + 2 * padding;
+
+    let mut cells = vec![false; size_x * size_y * size_z * size_w];
+
+    for (row_index, row) in plane_cells.chunks_exact(plane_width).enumerate() {
+        for (col_index, cell) in row.iter().enumerate() {
+            cells[size_x * size_y * size_z * padding
+                + size_x * size_y * padding
+                + size_x * (row_index + padding)
+                + col_index
+                + padding] = *cell;
+        }
+    }
+
+    World4d {
+        cells,
+        size_x,
+        size_y,
+        size_z,
+        size_w,
+    }
+}
+
 fn parse_input(input: &str) -> (Vec<bool>, usize) {
     // less error checking today, just assume the input is not malformed and all lines have the same length
     let width = input
@@ -133,21 +240,35 @@ fn parse_input(input: &str) -> (Vec<bool>, usize) {
 mod test {
     use super::*;
 
+    const EXAMPLE_INPUT: &str = r".#.
+..#
+###";
+
     #[test]
     fn run_generations_works_for_example() {
         // given
-        let (init_plane, init_width) = parse_input(
-            r".#.
-..#
-###",
-        );
+        let (init_plane, init_width) = parse_input(EXAMPLE_INPUT);
         let world = create_world_with_centered_plane(&init_plane, init_width, 6);
 
         // when
         let result_world = run_generations(world, 6);
-        let alive_count = count_alive_cells(&result_world);
+        let alive_count = count_alive_cells(&result_world.cells);
 
         // then
         assert_eq!(alive_count, 112);
+    }
+
+    #[test]
+    fn run_generations4d_works_for_example() {
+        // given
+        let (init_plane, init_width) = parse_input(EXAMPLE_INPUT);
+        let world = create_world4d_with_centered_plane(&init_plane, init_width, 6);
+
+        // when
+        let result_world = run_generations4d(world, 6);
+        let alive_count = count_alive_cells(&result_world.cells);
+
+        // then
+        assert_eq!(alive_count, 848);
     }
 }
