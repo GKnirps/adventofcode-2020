4f41a56c64e3f68976422caa45b390bf5205d654
Guido Knips <guido.knips@posteo.de>
2020-12-20 14:30:42 +0100

    Add solution for day 20, part 2
    friggin' off-by-one errors

--- day-20/src/main.rs
+++ day-20/src/main.rs
@@ -1,10 +1,12 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::env;
+use std::fmt;
 use std::fs::read_to_string;
 use std::path::Path;
 
 const TILE_SIZE: usize = 10;
+const TILE_SIZE_IMG: usize = TILE_SIZE - 2;
 
 fn main() -> Result<(), String> {
     let filename = env::args()
@@ -14,17 +16,163 @@ fn main() -> Result<(), String> {
     let tiles = parse_tiles(&content)?;
 
     let tile_variants = tiles_rot_flipped(tiles);
-    let img = solve_tile_puzzle(&tile_variants)?;
-    let corner_product = prod_corner_ids(&img);
+    let tiled_img = solve_tile_puzzle(&tile_variants)?;
+    let corner_product = prod_corner_ids(&tiled_img);
     println!(
         "Product of the corners of the solved puzzle: {}",
         corner_product
     );
 
+    let img = img_from_tiled_img(&tiled_img);
+    let roughness = count_water_roughness(&img);
+    println!("The water roughness value is {}", roughness);
+
     Ok(())
 }
 
-fn prod_corner_ids(img: &Img) -> u64 {
+#[derive(Clone, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
+struct Img {
+    img_data: Vec<bool>,
+    width: usize,
+    height: usize,
+}
+
+impl fmt::Display for Img {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for row in self.img_data.chunks_exact(self.width) {
+            for px in row {
+                if *px {
+                    write!(f, "#")?;
+                } else {
+                    write!(f, ".")?;
+                }
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
+// important assumption: sea serpents do not overlap
+fn count_water_roughness(photo: &Img) -> usize {
+    let patterns = create_all_sea_serpent_patterns();
+    let n_sea_serpents = count_sea_serpents(photo, &patterns);
+    let total_white_pixels = photo.img_data.iter().filter(|p| **p).count();
+    let sea_searpent_white_pixels = patterns[0].img_data.iter().filter(|p| **p).count();
+
+    total_white_pixels - n_sea_serpents * sea_searpent_white_pixels
+}
+
+// Important: counts overlapping sea serpents as well
+fn count_sea_serpents(photo: &Img, patterns: &[Img]) -> usize {
+    patterns
+        .iter()
+        .map(|pattern| {
+            let mut count: usize = 0;
+            for top in 0..=(photo.height - pattern.height) {
+                for left in 0..=(photo.width - pattern.width) {
+                    if pattern_matches_at_pos(photo, pattern, top, left) {
+                        count += 1;
+                    }
+                }
+            }
+            count
+        })
+        .max()
+        .unwrap_or(0)
+}
+
+fn pattern_matches_at_pos(img: &Img, pattern: &Img, top: usize, left: usize) -> bool {
+    if img.height < top + pattern.height || img.width < left + pattern.width {
+        return false;
+    }
+    for y in 0..pattern.height {
+        for x in 0..pattern.width {
+            if pattern.img_data[y * pattern.width + x]
+                && !img.img_data[left + x + img.width * (top + y)]
+            {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn create_all_sea_serpent_patterns() -> [Img; 8] {
+    let pattern = create_sea_serpent_pattern();
+    let rot90 = rot_img_counterclock(&pattern);
+    let rot180 = rot_img_counterclock(&rot90);
+    let rot270 = rot_img_counterclock(&rot180);
+
+    let flipped = flip_img_hor(&pattern);
+    let flip_rot90 = rot_img_counterclock(&flipped);
+    let flip_rot180 = rot_img_counterclock(&flip_rot90);
+    let flip_rot270 = rot_img_counterclock(&flip_rot180);
+
+    [
+        pattern,
+        rot90,
+        rot180,
+        rot270,
+        flipped,
+        flip_rot90,
+        flip_rot180,
+        flip_rot270,
+    ]
+}
+
+fn create_sea_serpent_pattern() -> Img {
+    let raw = "                  # #    ##    ##    ### #  #  #  #  #  #   ";
+    let img_data: Vec<bool> = raw.chars().map(|c| c == '#').collect();
+    Img {
+        img_data,
+        width: 20,
+        height: 3,
+    }
+}
+
+fn rot_img_counterclock(img: &Img) -> Img {
+    let img_data = rotate_img_data_counterclock(&img.img_data, img.width, img.height);
+    Img {
+        img_data,
+        width: img.height,
+        height: img.width,
+    }
+}
+
+fn flip_img_hor(img: &Img) -> Img {
+    let img_data = flip_img_data_hor(&img.img_data, img.width, img.height);
+    Img {
+        img_data,
+        width: img.width,
+        height: img.height,
+    }
+}
+
+fn img_from_tiled_img(tiled_img: &TiledImg) -> Img {
+    let width = tiled_img.width * TILE_SIZE_IMG;
+    let height = width;
+    let mut img_data = vec![false; width * height];
+    for tile_y in 0..tiled_img.width {
+        for tile_x in 0..tiled_img.width {
+            for tile_px_y in 0..TILE_SIZE_IMG {
+                for tile_px_x in 0..TILE_SIZE_IMG {
+                    let y = tile_y * TILE_SIZE_IMG + tile_px_y;
+                    let x = tile_x * TILE_SIZE_IMG + tile_px_x;
+                    img_data[x + y * width] = tiled_img.tiles[tile_x + tile_y * tiled_img.width]
+                        .img_data[tile_px_x + tile_px_y * TILE_SIZE_IMG];
+                }
+            }
+        }
+    }
+    Img {
+        img_data,
+        width,
+        height,
+    }
+}
+
+fn prod_corner_ids(img: &TiledImg) -> u64 {
     if img.width == 0 {
         return 1;
     }
@@ -79,7 +227,20 @@ fn rotate_tile_counterclock(tile: &Tile) -> Tile {
         border_left: flip_border(tile.border_top),
         border_bottom: tile.border_left,
         border_right: flip_border(tile.border_bottom),
+        img_data: rotate_img_data_counterclock(&tile.img_data, TILE_SIZE_IMG, TILE_SIZE_IMG),
     }
+}
+
+fn rotate_img_data_counterclock(data: &[bool], width: usize, height: usize) -> Vec<bool> {
+    let mut rotated: Vec<bool> = vec![false; width * height];
+    for y in 0..height {
+        for x in 0..width {
+            let rot_x = y;
+            let rot_y = width - 1 - x;
+            rotated[rot_x + rot_y * height] = data[x + y * width];
+        }
+    }
+    rotated
 }
 
 fn flip_tile_hor(tile: &Tile) -> Tile {
@@ -89,7 +250,20 @@ fn flip_tile_hor(tile: &Tile) -> Tile {
         border_left: tile.border_right,
         border_bottom: flip_border(tile.border_bottom),
         border_right: tile.border_left,
+        img_data: flip_img_data_hor(&tile.img_data, TILE_SIZE_IMG, TILE_SIZE_IMG),
     }
+}
+
+fn flip_img_data_hor(data: &[bool], width: usize, height: usize) -> Vec<bool> {
+    let mut flipped: Vec<bool> = vec![false; width * height];
+    for y in 0..height {
+        for x in 0..width {
+            let flip_x = width - 1 - x;
+            let flip_y = y;
+            flipped[flip_x + flip_y * width] = data[x + y * width];
+        }
+    }
+    flipped
 }
 
 fn flip_border(mut border: u16) -> u16 {
@@ -103,12 +277,12 @@ fn flip_border(mut border: u16) -> u16 {
 }
 
 #[derive(Clone, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
-struct Img<'a> {
+struct TiledImg<'a> {
     tiles: Vec<&'a Tile>,
     width: usize,
 }
 
-fn solve_tile_puzzle(tiles: &[Tile]) -> Result<Img, String> {
+fn solve_tile_puzzle(tiles: &[Tile]) -> Result<TiledImg, String> {
     let img_width = stupid_int_sqrt(tiles.len() / 8)
         .ok_or_else(|| format!("Cannot order {} tiles into a square", tiles.len() / 8))?;
     let tiles_by_top: HashMap<u16, Vec<&Tile>> =
@@ -152,7 +326,7 @@ fn solve_tile_puzzle(tiles: &[Tile]) -> Result<Img, String> {
             &tiles_by_left,
             &tiles_by_top_left,
         ) {
-            return Ok(Img {
+            return Ok(TiledImg {
                 width: img_width,
                 tiles: ordered_tiles,
             });
@@ -311,6 +485,7 @@ fn parse_tile(input: &str) -> Result<Tile, String> {
             border_right |= 1;
         }
     }
+    let img_data = crop_tile_image_data(&pixel);
 
     Ok(Tile {
         id,
@@ -318,7 +493,18 @@ fn parse_tile(input: &str) -> Result<Tile, String> {
         border_left,
         border_bottom,
         border_right,
+        img_data,
     })
+}
+
+fn crop_tile_image_data(full_tile: &[bool]) -> Vec<bool> {
+    (0..TILE_SIZE_IMG * TILE_SIZE_IMG)
+        .map(|i| {
+            let x = i % TILE_SIZE_IMG + 1;
+            let y = i / TILE_SIZE_IMG + 1;
+            full_tile[x + y * TILE_SIZE]
+        })
+        .collect()
 }
 
 #[derive(Clone, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
@@ -328,6 +514,7 @@ struct Tile {
     border_left: u16,
     border_bottom: u16,
     border_right: u16,
+    img_data: Vec<bool>,
 }
 
 #[cfg(test)]
@@ -447,13 +634,21 @@ Tile 3079:
     #[test]
     fn rotate_tile_counterclock_rotates_correctly() {
         // given
+        let mut img_data = vec![false; TILE_SIZE_IMG * TILE_SIZE_IMG];
+        img_data[0] = true;
+        img_data[TILE_SIZE_IMG + 1] = true;
         let tile = Tile {
             id: 42,
             border_top: 0b0001,
             border_left: 0b0011,
             border_bottom: 0b0111,
             border_right: 0b1111,
+            img_data,
         };
+
+        let mut expected_img_data = vec![false; TILE_SIZE_IMG * TILE_SIZE_IMG];
+        expected_img_data[(TILE_SIZE_IMG - 1) * TILE_SIZE_IMG] = true;
+        expected_img_data[1 + TILE_SIZE_IMG * (TILE_SIZE_IMG - 2)] = true;
 
         // when
         let rotated = rotate_tile_counterclock(&tile);
@@ -466,6 +661,7 @@ Tile 3079:
                 border_left: 0b1000000000,
                 border_bottom: 0b0000000011,
                 border_right: 0b1110000000,
+                img_data: expected_img_data,
             }
         )
     }
@@ -473,13 +669,21 @@ Tile 3079:
     #[test]
     fn flip_tile_hor_flips_correctly() {
         // given
+        let mut img_data = vec![false; TILE_SIZE_IMG * TILE_SIZE_IMG];
+        img_data[0] = true;
+        img_data[TILE_SIZE_IMG + 1] = true;
         let tile = Tile {
             id: 42,
             border_top: 0b0001,
             border_left: 0b0011,
             border_bottom: 0b0111,
             border_right: 0b1111,
+            img_data,
         };
+
+        let mut expected_img_data = vec![false; TILE_SIZE_IMG * TILE_SIZE_IMG];
+        expected_img_data[(TILE_SIZE_IMG - 1)] = true;
+        expected_img_data[TILE_SIZE_IMG + (TILE_SIZE_IMG - 2)] = true;
 
         // when
         let flipped = flip_tile_hor(&tile);
@@ -493,6 +697,7 @@ Tile 3079:
                 border_left: 0b1111,
                 border_bottom: 0b1110000000,
                 border_right: 0b0011,
+                img_data: expected_img_data,
             }
         )
     }
@@ -510,5 +715,21 @@ Tile 3079:
         assert_eq!(result.width, 3);
         assert_eq!(result.tiles.len(), 9);
         assert_eq!(prod_corner_ids(&result), 20899048083289);
+    }
+
+    #[test]
+    fn count_water_roughness_works_for_example() {
+        // given
+        let tiles = parse_tiles(EXAMPLE_INPUT).expect("expected example input to parse");
+        let tile_variants = tiles_rot_flipped(tiles);
+        let tiled_img = solve_tile_puzzle(&tile_variants).expect("expected puzzle to be solved");
+        let img = img_from_tiled_img(&tiled_img);
+
+        // when
+        println!("{}", img);
+        let roughness = count_water_roughness(&img);
+
+        // then
+        assert_eq!(roughness, 273);
     }
 }
